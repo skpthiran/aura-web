36772d1341a3e658761732c72cf6a14b413e673b
Thiran Wijesinhga <skpthiran@gmail.com>
2026-05-05 23:39:40 +0530

    fix: remove redundant AuthProvider to fix double-mounting redirect loop

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <App />
 )
 
 // cache-bust 2026-04-21 08:07:45
